12dd2d763c0430fa5e361906cb5e6573c6d4e1cf
LaurenR01 <laurenrowe01@gmail.com>
2021-08-08 15:59:09 -0400

    fixed errors

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-app.use('apiRoutes.js', apiRoutes)(app);
-app.use('htmlRoutes.js', htmlRoutes)(app);
+app.use('apiRoutes.js', apiRoutes);
+app.use('htmlRoutes.js', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
