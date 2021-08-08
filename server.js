8a52951b71e664c8c4ffbff049919182656a487b
LaurenR01 <laurenrowe01@gmail.com>
2021-08-08 15:31:12 -0400

    fixing routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const PORT = process.env.PORT || 3301;
 
@@ -8,8 +10,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-require ('./routes/apiRoutes')(app);
-require ('./routes/htmlRoutes')(app);
+
+app.use('apiRoutes.js', apiRoutes)(app);
+app.use('htmlRoutes.js', htmlRoutes)(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
